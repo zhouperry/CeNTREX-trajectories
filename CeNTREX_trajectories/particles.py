0c4224b7135168c8757351a50f8059fff4315885
ograsdijk <o.grasdijk@gmail.com>
2022-05-28 14:05:21 -0500

    Add magnetic moment and BaF

--- CeNTREX_trajectories/particles.py
+++ CeNTREX_trajectories/particles.py
@@ -1,14 +1,22 @@
+import math
 from abc import ABC
 from dataclasses import dataclass
 
-__all__ = ["TlF"]
+__all__ = ["Particle", "TlF", "BaF"]
 
 
 @dataclass
 class Particle(ABC):
     mass: float
+    magnetic_moment: float = math.nan
 
 
 @dataclass
 class TlF(Particle):
     mass: float = (204.38 + 19.00) * 1.67e-27  # mass in kg
+
+
+@dataclass
+class BaF(Particle):
+    mass: float = (137 + 19) * 1.67e-27  # mass in kg
+    magnetic_moment: float = 9.274e-24  # magnetic moment in J/T
